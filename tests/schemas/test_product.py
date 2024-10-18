3e081ed32644b3199437cca774e310a0f7bf8219
amanmdest <amanmdest@gmail.com>
2024-10-18 09:36:21 -0300

    Rodando pre-commit

--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -10,24 +10,23 @@ def test_schemas_return_success():
     data = product_data()
     product = ProductIn.model_validate(data)
 
-    assert product.name == 'Iphone 14 pro Max'
+    assert product.name == "Iphone 14 pro Max"
     assert isinstance(product.id, UUID)
+
 
 def test_schemas_return_raise():
     data = {
-        'name':'Iphone 14 pro Max',
-        'quantity':'10', 
-        'price':'8500', 
+        "name": "Iphone 14 pro Max",
+        "quantity": "10",
+        "price": "8500",
     }
     with pytest.raises(ValidationError) as err:
         ProductIn.model_validate(data)
 
     assert err.value.errors()[0] == {
-        'type': 'missing', 
-        'loc': ('status',), 
-        'msg': 'Field required', 
-        'input': {
-            'name': 'Iphone 14 pro Max', 
-            'quantity': '10', 
-            'price': '8500'}, 
-            'url': 'https://errors.pydantic.dev/2.9/v/missing'}
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "Iphone 14 pro Max", "quantity": "10", "price": "8500"},
+        "url": "https://errors.pydantic.dev/2.9/v/missing",
+    }
