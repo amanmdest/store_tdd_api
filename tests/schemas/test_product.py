f9a727735f151105d7838a7e4d36bc9d0a14fc2c
amanmdest <amanmdest@gmail.com>
2024-10-19 22:07:47 -0300

    fixing typos, makefile and pre-commit

--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -27,6 +27,10 @@ def test_schemas_return_raise():
         "type": "missing",
         "loc": ("status",),
         "msg": "Field required",
-        "input": {"name": "Iphone 14 pro Max", "quantity": "10", "price": "8500"},
+        "input": {
+            "name": "Iphone 14 pro Max",
+            "quantity": "10",
+            "price": "8500",
+        },
         "url": "https://errors.pydantic.dev/2.9/v/missing",
     }
